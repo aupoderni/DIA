27fdae841333ba29f80623268605f2cd4d48ebca
aupoderni <72687246+aupoderni@users.noreply.github.com>
2023-06-28 16:12:19 +0700

    Update openms_load.py

--- Slider_DIA/openms_load.py
+++ Slider_DIA/openms_load.py
@@ -7,9 +7,80 @@ import torch.optim as optim
 import source.models as models
 import source.parameters as params
 
+def discretize_spectrum(spectrum, mass_proton, bin_width, bin_offset):
+    peaks = spectrum.get_peaks()
+    peak_bins = list(map(lambda p: mass2bin(p, mass_proton, bin_width, bin_offset), peaks[0]))
+    bins = np.zeros(peak_bins[-1] + 1, dtype='float64')
+    for peak_idx, peak in enumerate(peak_bins):
+        if peak < (peak_bins[-1] + 1) and bins[peak] < peaks[1][peak_idx]:
+            bins[peak] = peaks[1][peak_idx]
+    return bins
+
+def undiscretize_spectrum(bins, mass_proton, bin_width, bin_offset):
+    mz = []
+    intensity = []
+    peak_bin_idx = np.nonzero(bins)
+    bin_to_peak = [bin2mass(peak_bin, mass_proton, bin_width, bin_offset)
+                    for peak_bin in peak_bin_idx[0]]
+    for i, peak in enumerate(bin_to_peak):
+        peak_intensity = bins[peak_bin_idx[0][i]]
+        if peak > 0:
+            mz.append(peak)
+            intensity.append(peak_intensity)
+    return mz, intensity
+
+def create_oms_spectrum(mz, intensity, spectrum):
+    oms_spectrum = oms.MSSpectrum()
+    oms_spectrum.set_peaks([mz, intensity])
+    oms_spectrum.setMSLevel(spectrum.getMSLevel())
+    oms_spectrum.setRT(spectrum.getRT())
+    oms_spectrum.setPrecursors(spectrum.getPrecursors())
+    oms_spectrum.setInstrumentSettings(spectrum.getInstrumentSettings())
+    return oms_spectrum
+
+def mass2bin(mass, mass_proton, bin_width, bin_offset,  charge=1):
+    return ((mass + (charge - 1) * mass_proton)/
+            (charge*bin_width) + 1.0 - bin_offset).astype(int)
+
+def bin2mass(bin, mass_proton, bin_width, bin_offset, charge=1):
+        """Convert bin to mass"""
+        return ((bin - 1.0 + bin_offset) * (charge * bin_width)
+                 + (charge - 1) * mass_proton)
+
+#set parameters
+mass_proton = 1.00727646688
+bin_width = 0.01
+bin_offset = 0.0
+
+#create oms experiment and load the data
 exp = oms.MSExperiment()
 oms.MzXMLFile().load("data\e01306.mzXML", exp)
 
+#create another oms experiment for data export
+exp_outp = oms.MSExperiment()
+
+for cnt, _ in enumerate(exp):
+    if exp[cnt].getMSLevel() == 1:
+        exp_outp.addSpectrum(exp[cnt])
+        continue
+    
+    bins = discretize_spectrum(exp[cnt], mass_proton, bin_width, bin_offset)
+
+    #binned spectrum to mz&intensity
+    mz, intensity = undiscretize_spectrum(bins, mass_proton, bin_width, bin_offset)
+
+    #recreate oms spectrum
+    oms_spectrum = create_oms_spectrum(mz, intensity, exp[cnt])
+    exp_outp.addSpectrum(oms_spectrum)
+
+    if cnt == 1:
+        print(exp[0].get_peaks()[0])
+        print(exp_outp[0].get_peaks()[0])
+        print(exp[1].get_peaks()[0])
+        print(exp_outp[1].get_peaks()[0])
+oms.MzXMLFile().store('output.mzXML', exp_outp)
+
+'''
 spy = Spytrometer.Spytrometer(max_peak=2000, bin_width=0.01, bin_offset=0.0, remove_precursor_peak=False, skip_preprocessing=True)
 
 #spy.bin_width = 0.2
@@ -31,7 +102,7 @@ for spectrum_id, spectrum in enumerate(spy.spectrum_collection):
     #spy.normalize_regions(spectrum_id)
 
 # Make a dict with precursor masses as keys and spectrum index lists with these precursor masses as values
-'''
+
 prec_masses = {}
 for i in range(len(spy.spectrum_collection)):
     if spy.spectrum_collection[i].precursor_mass not in prec_masses.keys():
@@ -56,4 +127,4 @@ for key, value in prec_masses.items():
         spy.spectrum_collection[value[i]].spectrum_array = spectrum_collection_concat[i].tolist()
 '''
 # Save new data as .mzxml file
-spy.export_spectra_mzxml('output.mzXML', exp)
+#spy.export_spectra_mzxml('output.mzXML', exp)
